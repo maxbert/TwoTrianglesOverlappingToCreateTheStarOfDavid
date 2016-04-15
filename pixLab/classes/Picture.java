4f16673db636c895c26ccf9cedaaf0ebd9337166
Max Bertfield <mbertfield@stuy.edu>
2016-04-15 14:25:14 -0400

    added A6

--- pixLab/classes/Picture.java
+++ pixLab/classes/Picture.java
@@ -117,7 +117,41 @@ public class Picture extends SimplePicture
       }
     } 
   }
-  
+
+      public void mirrorVerticalRightToLeft()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    Pixel leftPixel = null;
+    Pixel rightPixel = null;
+    int width = pixels[0].length;
+    for (int row = pixles.length; row > 0; row--)
+    {
+      for (int col = 0; col < width / 2; col++)
+      {
+        leftPixel = pixels[row][col];
+        rightPixel = pixels[row][width - 1 - col];
+        rightPixel.setColor(leftPixel.getColor());
+      }
+    } 
+  }
+
+          public void mirrorVerticalBottomToTop()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    Pixel leftPixel = null;
+    Pixel rightPixel = null;
+    int width = pixels[0].length;
+    for (int row = pixles.length; row > 0; row--)
+    {
+      for (int col = width / 2; col > 0; col--)
+      {
+        leftPixel = pixels[row][col];
+        rightPixel = pixels[row][width - 1 - col];
+        rightPixel.setColor(leftPixel.getColor());
+      }
+    } 
+  }
+
   /** Mirror just part of a picture of a temple */
   public void mirrorTemple()
   {
